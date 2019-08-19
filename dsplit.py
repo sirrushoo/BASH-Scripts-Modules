c59e8157d5c6b2053a937b23b6323ba7737198c3
O.T Osman <sirrush.oo@gmail.com>
2019-08-19 11:30:49 -0500

    Create dsplit.py

---
+++ dsplit.py
@@ -0,0 +1,30 @@
+import sys,os
+
+# pydsplit.py [startoffset] [blocksize] [filename]
+
+startoffset = int(sys.argv[1])
+blocksize = int(sys.argv[2])
+filename = sys.argv[3]
+
+size = os.path.getsize(filename)
+
+fh = open(filename, "rb")
+nameonly = os.path.splitext(filename)[0]
+extonly = os.path.splitext(filename)[1]
+
+data = ""
+if startoffset > 0:
+	data = fh.read(startoffset)
+
+cursor = fh.tell()
+while cursor < size:
+	data += fh.read(blocksize)
+	newname = nameonly + "_" + str(len(data)) + extonly
+	cursor = fh.tell()
+	
+	print newname+". Size: "+str(len(data))
+	newfh = open(newname, "wb")
+	newfh.write(data)
+	newfh.close()
+
+fh.close()
